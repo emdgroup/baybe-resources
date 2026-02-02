5c608f0979e6b69ee1d2f848f26e1c92ef2c45ea
Alexander V. Hopp <alexander.hopp@merckgroup.com>
2026-02-02 14:10:33 +0100

    Update Chemical_Encodings notebook

--- notebooks/Chemical_Encodings.py
+++ notebooks/Chemical_Encodings.py
@@ -18,9 +18,9 @@ def _(mo):
     mo.md("""
     # Chemical encodings
 
-    This file contains some examples related to chemical encodings. 
-    
-    Note that this example assumes some basic familiarity with using `BayBE`, and that it does not explain all of the core concepts. If you are interested in those, we recommend to first check out the `Reation_Optimization` example.
+    This notebook demonstrates the power and usefulness of BayBE's chemical encodings. If parameters in a process to be optimized are chemicals, this feature enables BayBE to automatically use meaningful chemical descriptors, automatically leveraging chamical knowledge for the optimization process.
+
+    This notebook assumes some basic familiarity with using BayBE, and that it does not explain all of the core concepts. If you are interested in those, we recommend to first check out the `Reation_Optimization` example.
     """)
     return
 
@@ -30,7 +30,7 @@ def _(mo):
     mo.md("""
     ## Setup
 
-    We begin this example by defining a suitable recommender. We use a `TwoPhaseMetaRecommender` equipped with a `BotorchRecommender`. We also use use a specific kernel that is suited particularly well for chemical problems - the [`EDBOKernel`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.surrogates.gaussian_process.presets.edbo.EDBOKernelFactory.html#baybe.surrogates.gaussian_process.presets.edbo.EDBOKernelFactory).
+    We begin this example by defining a suitable recommender. We use a [`TwoPhaseMetaRecommender`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.recommenders.meta.sequential.TwoPhaseMetaRecommender.html) equipped with a [`BotorchRecommender`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.recommenders.pure.bayesian.botorch.BotorchRecommender.html). We also use use a specific kernel that is suited particularly well for chemical problems - the [`EDBOKernel`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.surrogates.gaussian_process.presets.edbo.EDBOKernelFactory.html#baybe.surrogates.gaussian_process.presets.edbo.EDBOKernelFactory).
     """)
     return
 
@@ -54,7 +54,9 @@ def _():
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    This examples uses the same basic example like the `Reaction_Optimization` notebook. However, we now use different encodings of the chemical parameters. The encoding that should be used can be described by the `encoding` field. We investigate three different encodings here and create one campaign per chemical encoding.
+    This examples uses the same chemical reaction like the `Reaction_Optimization` notebook. However, it uses different encodings of the chemical parameters. This is done by using BayBE's [`SubstanceParameter`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.parameters.substance.SubstanceParameter.html) which automatically translates chemicals into meaningful descriptors. For more details on the `SubstanceParameter`, we refer to the [corresponding section of the user guide](https://emdgroup.github.io/baybe/0.14.2/userguide/parameters.html#substanceparameter).
+
+    The specific encoding that should be used can be described by the `encoding` field. We investigate three different encodings here and create one campaign per chemical encoding.
     """)
     return
 
@@ -78,7 +80,7 @@ def _():
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    Now that we have the data collected, we can create different campaigns that we want to compare against each other. To facilitate the usage of `BayBE`'s simulation capabilities, we collect the different campaigns in a `dict`.
+    Now that we have collected the data, we can create different campaigns that we want to compare against each other. To facilitate the usage of `BayBE`'s simulation capabilities, we collect the different campaigns in a `dict`.
     """)
     return
 
@@ -180,6 +182,20 @@ def _(
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(r"""
+    /// note
+    Sometimes, none of the well-known encodings might be suitable for a specific use case. BayBE thus also allows to supply an arbitrary, user defined "translation" from chemicals to meaningful descriptors. More generally, the [`CustomDiscreteParameter`](https://emdgroup.github.io/baybe/0.14.2/_autosummary/baybe.parameters.custom.CustomDiscreteParameter.html#baybe.parameters.custom.CustomDiscreteParameter) allows fully user-specified representations of data.
+    ///
+
+    /// admonition | Task
+    Create an additional campaign using a `CustomDiscreteParameter` with some "dummy data" as a representation for one or more of the chemical substances. More information on this parameter can be found in the [corresponding section of the user guide](https://emdgroup.github.io/baybe/0.14.2/userguide/parameters.html#customdiscreteparameter).
+    ///
+    """)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
     ## Using `BayBE`'s [simulation capabilities](https://emdgroup.github.io/baybe/0.14.2/userguide/simulation.html)
 
     `BayBE` offers multiple functionalities to “simulate” experimental campaigns with a given lookup mechanism. `BayBE`’s simulation package enables a wide range of use cases and can even be used for “oracle predictions”. This is made possible through the flexible use of lookup mechanisms, which act as the loop-closing element of an optimization loop.
@@ -187,6 +203,10 @@ def _(mo):
     Lookups can be provided in a variety of ways, by using fixed data sets, analytical functions, or any other form of black-box callable. In all cases, their role is the same: to retrieve target values for parameter configurations suggested by the recommendation engine.
 
     In our case, we can directly use the data that we stored in the `df` dataframe and do the simulation.
+
+    /// Note
+    For performance reasons, the default values used in this example are very low. We recommend to increase `N_DOE_ITERATIONS` resp. `N_MC_ITERATIONS` to values of at least 15 resp. 25 to observe the effects of the different encodings. For best results, increase them to 25 resp. 40.
+    ///
     """)
     return
 
@@ -196,8 +216,8 @@ def _(df, scenarios):
     from baybe.simulation import simulate_scenarios
 
     BATCH_SIZE = 2
-    N_DOE_ITERATIONS = 10  # Change to ~15 for better plots
-    N_MC_ITERATIONS = 15  # Change to ~25 for better plots
+    N_DOE_ITERATIONS = 5
+    N_MC_ITERATIONS = 10
 
     results = simulate_scenarios(
         scenarios,
@@ -221,7 +241,7 @@ def _(df, scenarios):
 @app.cell(hide_code=True)
 def _(mo):
     mo.md("""
-    We now visualize the results using the `backtest_plot` utility. This utility "averages" the individual Monte Carlo iterations and shows the mean and a confidence interval. It can also be used to give visual guidance on the performance of individual scenarios.
+    We now visualize the results using the `backtest_plot` utility. This utility "averages" the individual Monte Carlo iterations and shows the mean and a confidence interval. It can also be used to give visual guidance on the performance of individual scenarios. We mark the (fictional) goal of a yield of at least 90% and compare the different encodings.
     """)
     return
 
@@ -236,8 +256,7 @@ def _(mo, results):
         x="Number of experiments",
         y="Running best yield",
         hue="Substance encoding",
-        indicator_y=90,
-        indicator_labels=["MORDRED"],
+        indicator_y=90
     )
     mo.mpl.interactive(plt.gcf())
     return (plt,)
@@ -246,6 +265,14 @@ def _(mo, results):
 @app.cell
 def _(plt):
     plt.close()
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    The plot demonstrates that all of the chemical encodings outperform the one-hot-encoding. Even though all of the encodings perform comparably during the first few iterations, the campaigns leveraging the chemical encodings reach the marked target of achieving at least 90% yield significantly faster.
+    """)
     return
 
 
